f4e3e203c2ff2c3842083861a7c74ca430ee578b
Krishen Rana <krishenrana@me.com>
2018-04-13 09:30:23 +0000

    Requirements.txt django-countries

--- edm_music_app/settings/base.py
+++ edm_music_app/settings/base.py
@@ -23,9 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['project-3-krontime.c9users.io',]
 
 
